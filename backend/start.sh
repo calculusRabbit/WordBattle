3abe88c4ab7f99d16e6d3e29f7b04ebbabd949e6
Vu Nguyen <vunguyenkyphat@gmail.com>
2026-05-21 21:40:09 -0500

    use huggingface for glove download

--- backend/start.sh
+++ backend/start.sh
@@ -5,7 +5,7 @@ WORDS_PATH="src/model/words_filtered.txt"
 
 if [ ! -f "$GLOVE_PATH" ]; then
     echo "Downloading GloVe file..."
-    wget -O "$GLOVE_PATH" "https://drive.google.com/uc?export=download&id=1QHrrXxViYH7X0xqHz7CsbdVuEu-fY740"
+    wget -O "$GLOVE_PATH" "https://huggingface.co/datasets/calculusrabbit/wordduel-model/resolve/main/wiki_giga_2024_100_MFT20_vectors_seed_2024_alpha_0.75_eta_0.05.050_combined.txt"
 fi
 
 if [ ! -f "$WORDS_PATH" ]; then
