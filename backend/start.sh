46e37b067401335bff72d8ddcb7ba54b81c103c1
Vu Nguyen <vunguyenkyphat@gmail.com>
2026-05-21 20:39:02 -0500

    add github action workflow and start up script

---
+++ backend/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+GLOVE_PATH="src/model/wiki_giga_2024_100_MFT20_vectors_seed_2024_alpha_0.75_eta_0.05.050_combined.txt"
+WORDS_PATH="src/model/words_filtered.txt"
+
+if [ ! -f "$GLOVE_PATH" ]; then
+    echo "Downloading GloVe file..."
+    wget -O "$GLOVE_PATH" "https://drive.google.com/uc?export=download&id=1QHrrXxViYH7X0xqHz7CsbdVuEu-fY740"
+fi
+
+if [ ! -f "$WORDS_PATH" ]; then
+    echo "Downloading filtered words..."
+    wget -O "$WORDS_PATH" "https://drive.google.com/uc?export=download&id=1ljTniifH2sPA3SRmbgsDuCMqb5QC853D"
+fi
+
+exec node src/server.js
